eab2f6bd62ddd32cafa71069a67722f8c6ac8684
cdclark <cdclark@users.noreply.github.com>
2014-12-02 12:03:58 -0500

    Create allEmployeeSearch.php

---
+++ allEmployeeSearch.php
@@ -0,0 +1,183 @@
+<html>
+
+<title>Employee Information</title>
+
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+	<!-- Header Banner-->
+	<?php
+		include('./header.php');
+	?>
+	<!-- End Header Banner-->	
+
+<body>
+<h4>Select up to 5 search fields and search terms</h4>
+<br>
+<form action="skilllist.html" method="post">
+<div class="dataentry">
+
+<table class="search">
+    <tr>
+    <td></td>
+    <td>Field to Search</td>
+    <td>Search Term</td>
+    </tr>
+    
+    <tr>       
+    <td>1</td>
+    <td>
+<select>
+    <option value="0"></option>
+    <option value="1">Name</option>
+    <option value="2">Employee ID</option> 
+    <option value="3">Title</option> 
+    <option value="4">Level</option> 
+    <option value="5">Team</option> 
+    <option value="6">Company Region</option> 
+    <option value="7">Review Period Start</option> 
+    <option value="8">Review Period End</option> 
+    <option value="9">Review Rating</option>
+    <option value="10">Review Author</option>
+    <option value="11">History Start Date</option>
+    <option value="12">History End Date</option> 
+    <option value="13">History Title</option> 
+    <option value="14">History Level</option> 
+    <option value="15">School</option> 
+    <option value="16">Degree</option> 
+    <option value="17">Major</option> 
+    <option value="18">Skill</option> 
+    <option value="19">Skill Level</option> 
+</select>
+    </td>
+    <td><input type="text" name="emptitle" value=""></td>
+    </tr>
+    
+    <tr>       
+    <td>2</td>
+    <td>
+<select>
+    <option value="0"></option>
+    <option value="1">Name</option>
+    <option value="2">Employee ID</option> 
+    <option value="3">Title</option> 
+    <option value="4">Level</option> 
+    <option value="5">Team</option> 
+    <option value="6">Company Region</option> 
+    <option value="7">Review Period Start</option> 
+    <option value="8">Review Period End</option> 
+    <option value="9">Review Rating</option>
+    <option value="10">Review Author</option>
+    <option value="11">History Start Date</option>
+    <option value="12">History End Date</option> 
+    <option value="13">History Title</option> 
+    <option value="14">History Level</option> 
+    <option value="15">School</option> 
+    <option value="16">Degree</option> 
+    <option value="17">Major</option> 
+    <option value="18">Skill</option> 
+    <option value="19">Skill Level</option> 
+</select>
+    </td>
+    <td><input type="text" name="emptitle" value=""></td>
+    </tr>
+    
+    <tr>       
+    <td>3</td>
+    <td>
+<select>
+    <option value="0"></option>
+    <option value="1">Name</option>
+    <option value="2">Employee ID</option> 
+    <option value="3">Title</option> 
+    <option value="4">Level</option> 
+    <option value="5">Team</option> 
+    <option value="6">Company Region</option> 
+    <option value="7">Review Period Start</option> 
+    <option value="8">Review Period End</option> 
+    <option value="9">Review Rating</option>
+    <option value="10">Review Author</option>
+    <option value="11">History Start Date</option>
+    <option value="12">History End Date</option> 
+    <option value="13">History Title</option> 
+    <option value="14">History Level</option> 
+    <option value="15">School</option> 
+    <option value="16">Degree</option> 
+    <option value="17">Major</option> 
+    <option value="18">Skill</option> 
+    <option value="19">Skill Level</option> 
+</select>
+    </td>
+    <td><input type="text" name="emptitle" value=""></td>
+    </tr>
+    
+    <tr>       
+    <td>4</td>
+    <td>
+<select>
+    <option value="0"></option>
+    <option value="1">Name</option>
+    <option value="2">Employee ID</option> 
+    <option value="3">Title</option> 
+    <option value="4">Level</option> 
+    <option value="5">Team</option> 
+    <option value="6">Company Region</option> 
+    <option value="7">Review Period Start</option> 
+    <option value="8">Review Period End</option> 
+    <option value="9">Review Rating</option>
+    <option value="10">Review Author</option>
+    <option value="11">History Start Date</option>
+    <option value="12">History End Date</option> 
+    <option value="13">History Title</option> 
+    <option value="14">History Level</option> 
+    <option value="15">School</option> 
+    <option value="16">Degree</option> 
+    <option value="17">Major</option> 
+    <option value="18">Skill</option> 
+    <option value="19">Skill Level</option> 
+</select>
+    </td>
+    <td><input type="text" name="emptitle" value=""></td>
+    </tr>
+
+    <tr>       
+    <td>5</td>
+    <td>
+<select>
+    <option value="0"></option>
+    <option value="1">Name</option>
+    <option value="2">Employee ID</option> 
+    <option value="3">Title</option> 
+    <option value="4">Level</option> 
+    <option value="5">Team</option> 
+    <option value="6">Company Region</option> 
+    <option value="7">Review Period Start</option> 
+    <option value="8">Review Period End</option> 
+    <option value="9">Review Rating</option>
+    <option value="10">Review Author</option>
+    <option value="11">History Start Date</option>
+    <option value="12">History End Date</option> 
+    <option value="13">History Title</option> 
+    <option value="14">History Level</option> 
+    <option value="15">School</option> 
+    <option value="16">Degree</option> 
+    <option value="17">Major</option> 
+    <option value="18">Skill</option> 
+    <option value="19">Skill Level</option> 
+</select>
+    </td>
+    <td><input type="text" name="emptitle" value=""></td>
+    </tr>
+    
+</table>
+    
+<br><br><br>
+</div>
+<input class="submitbutton" type="submit" value="Search Employees">
+</form>
+
+</body>
+</html>

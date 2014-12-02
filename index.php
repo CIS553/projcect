38780040f620390273bff18448ac97d54375635d
cdclark <cdclark@users.noreply.github.com>
2014-12-02 11:48:57 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+
+	<title>Employee Information</title>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+
+	<body>
+		<div id="MainIndexDiv">
+			<!-- Header Banner-->
+      <?php
+      include('./header.php');
+      ?>
+			<!--end Header Banner-->
+
+			<br>
+			<h1>Welcome to the Employee Information System</h1>
+			
+			<!--use instructions-->
+			<br>
+			<h4>Select an option above to continue</h4>
+			<ul>
+			    <li><b>Add Employee</b> : Enter information for a new employee record</li>
+			    <li><b>Skillset Lookup</b> : Locate employees with specific skills</li>
+			    <li><b>Search</b> : Search employee information</li>
+			</ul>
+			<br>
+			To update an existing employee, first search for the employee, then select the employee record and update
+			<!--end use instructions-->
+
+		</div>
+	</body>
+</html>

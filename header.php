92ff3388b54d7a8a6901b58a7addb9b7d05c76a7
cdclark <cdclark@users.noreply.github.com>
2014-12-02 11:49:19 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,10 @@
+<div id="headerDiv" name="headerDiv">
+	<table>
+		<tr>
+                        <td><a href="./index.php"> Home</a></td>
+			<td><a href="./addEmployee.php"> Add Employee</a></td>
+			<td><a href="./skillsetSearch.php"> Skillset Lookup</a></td>
+			<td><a href="./allEmployeeSearch.php"> Search</a></td>
+		</tr>
+	</table>
+</div>
